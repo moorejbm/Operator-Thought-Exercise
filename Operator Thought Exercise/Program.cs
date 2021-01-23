8d1ed8cc8e6b5c7c1427b84ffff124c71061522c
Jason Moore <moorejbm@gmail.com>
2021-01-22 18:20:29 -0600

    OpThaEX

---
+++ Operator Thought Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Operator_Thought_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            x *= 5;
+
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+
+            int age = 7;
+            int y = 4;
+
+            bool isTeenager = age > 12 && age < 20;
+
+            Console.WriteLine(k++);                        
+            Console.WriteLine(k);
+            Console.WriteLine(--k);
+            Console.WriteLine(--k);
+            Console.WriteLine(--k);
+
+            Console.WriteLine(isTeenager);
+
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
+
